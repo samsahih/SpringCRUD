d05ce6bb74ebf5daa474585b75b6092d41797893
samsahih <samersahih@gmail.com>
2024-04-15 16:30:48 +0300

    initial project setup and employee table creation

---
+++ src/main/java/com/crudexample/crud/controllers/EmployeeController.java
@@ -0,0 +1,7 @@
+package com.crudexample.crud.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+}
---
+++ src/main/java/com/crudexample/crud/repositories/IEmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.crudexample.crud.repositories;
+
+import com.crudexample.crud.entities.Employee;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IEmployeeRepository extends CrudRepository<Employee, Integer> {
+}
